d79991f64bf2e56ef1b057021ce0f688ce41b44c
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2024-05-08 21:25:44 +0200

    Add newly-generated gem

---
+++ spec/decoding_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Decoding do
+  it "has a version number" do
+    expect(Decoding::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
