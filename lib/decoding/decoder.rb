7fd4f3e5f8d18d56277079f74f63ba6f988b23d0
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2024-05-09 11:14:11 +0200

    Tweak Rubocop config

--- lib/decoding/decoder.rb
+++ lib/decoding/decoder.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 module Decoding
+  # A decoder is a callable object that reads any input value and returns an
+  # optionally transformed value.
+  #
+  # Note: this is an abstract class; use any of its subclasses instead of this
+  # class directly.
   class Decoder
     # @param value [Object]
     # @return [Decoding::Result<Object>]
