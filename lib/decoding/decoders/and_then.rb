1e511d98ff213627286335037c043466dba83a61
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2024-05-13 19:23:56 +0200

    Namespace and_then decoder

--- lib/decoding/decoders/and_then.rb
+++ lib/decoding/decoders/and_then.rb
@@ -3,21 +3,23 @@
 require_relative "../decoder"
 
 module Decoding
-  # Create a decoder that depends on a previously decoded value.
-  class AndThen < Decoder
-    # @param decoder [Decoding::Decoder<a>]
-    # @yieldparam [a]
-    # @yieldreturn [Decoding::Decoder<b>]
-    # @return [Decoding::Decoder<b>]
-    def initialize(decoder, &block)
-      @decoder = decoder.to_decoder
-      @block = block
-      super()
-    end
+  module Decoders
+    # Create a decoder that depends on a previously decoded value.
+    class AndThen < Decoder
+      # @param decoder [Decoding::Decoder<a>]
+      # @yieldparam [a]
+      # @yieldreturn [Decoding::Decoder<b>]
+      # @return [Decoding::Decoder<b>]
+      def initialize(decoder, &block)
+        @decoder = decoder.to_decoder
+        @block = block
+        super()
+      end
 
-    def call(value)
-      @decoder.call(value).and_then do |decoded_value|
-        @block.call(decoded_value).call(value)
+      def call(value)
+        @decoder.call(value).and_then do |decoded_value|
+          @block.call(decoded_value).call(value)
+        end
       end
     end
   end
